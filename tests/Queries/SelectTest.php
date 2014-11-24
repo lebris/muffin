db38404d156a7e5ab06031901566e5c58185d965
lebris <claude.lebris@gmail.com>
2014-11-24 21:55:49 +0100

    Add tests for select query

--- tests/Queries/SelectTest.php
+++ tests/Queries/SelectTest.php
@@ -27,4 +27,34 @@ class SelectTest extends PHPUnit_Framework_TestCase
 
         $this->assertSame("SELECT id, name FROM poney WHERE name = 'burger'", $query->toString($this->escaper));
     }
+
+    public function testSelectMultipleWhere()
+    {
+        $query = (new Queries\Select())->setEscaper($this->escaper);
+
+        $query
+            ->select(array('id', 'name'))
+            ->from('poney')
+            ->where(new Conditions\Equal('name', new Types\String('burger')))
+            ->where(new Conditions\Equal('rank', new Types\Int(42)))
+        ;
+
+        $this->assertSame("SELECT id, name FROM poney WHERE name = 'burger' AND rank = 42", $query->toString($this->escaper));
+    }
+
+    public function testSelectMultipleSelect()
+    {
+        $query = (new Queries\Select())->setEscaper($this->escaper);
+
+        $query
+            ->select(array('id', 'name'))
+            ->select('rank')
+            ->select(array('taste', 'price'))
+            ->select('rank')
+            ->from('poney')
+            ->where(new Conditions\Equal('name', new Types\String('burger')))
+        ;
+
+        $this->assertSame("SELECT id, name, rank, taste, price FROM poney WHERE name = 'burger'", $query->toString($this->escaper));
+    }
 }
