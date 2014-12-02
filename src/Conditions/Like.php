10118600d5be8f44a83b3ca6419b3ddb385156ba
lebris <claude.lebris@gmail.com>
2014-12-02 19:32:54 +0100

    Add Like condition

---
+++ src/Conditions/Like.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Mdd\QueryBuilder\Conditions;
+
+use Mdd\QueryBuilder\Condition;
+use Mdd\QueryBuilder\Type;
+use Mdd\QueryBuilder\Escaper;
+
+class Like extends AbstractCondition
+{
+    private
+        $column,
+        $type;
+
+    public function __construct($column, Type $type)
+    {
+        $this->column = (string) $column;
+        $this->type = $type;
+    }
+
+    public function toString(Escaper $escaper)
+    {
+        $value = $this->escapeValue($this->type->getValue(), $escaper);
+
+        if(empty($this->column))
+        {
+            return '';
+        }
+
+        return sprintf('%s LIKE %s', $this->column, $value);
+    }
+
+    private function escapeValue($value, Escaper $escaper)
+    {
+        if($this->type->isEscapeRequired())
+        {
+            $value = $escaper->escape($value);
+        }
+
+        return $value;
+    }
+}
