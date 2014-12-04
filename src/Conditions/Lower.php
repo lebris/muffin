7d5f84c47158a6962faf0bc6e98f37b096b36a50
Claude LE BRIS <claude.lebris@gmail.com>
2014-12-04 13:41:34 +0100

    Add Condition\Lower

---
+++ src/Conditions/Lower.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Mdd\QueryBuilder\Conditions;
+
+use Mdd\QueryBuilder\Type;
+use Mdd\QueryBuilder\Escaper;
+
+class Lower extends AbstractCondition
+{
+    private
+        $column,
+        $type;
+
+    public function __construct($column, Type $type)
+    {
+        $this->column = (string) $column;
+        $this->type = $type;
+    }
+
+    public function toString(Escaper $escaper)
+    {
+        $value = $this->escapeValue($this->type->getValue(), $escaper);
+
+        if(empty($this->column))
+        {
+            return '';
+        }
+
+        return sprintf('%s < %s', $this->column, $value);
+    }
+
+    private function escapeValue($value, Escaper $escaper)
+    {
+        if($this->type->isEscapeRequired())
+        {
+            $value = $escaper->escape($value);
+        }
+
+        return $value;
+    }
+}
