f6c91c644f0d82be94932d1282a564b46ebe8c67
Claude LE BRIS <claude.lebris@gmail.com>
2014-12-14 23:34:40 +0100

    Add QueryBuilder

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -2,7 +2,27 @@
 
 namespace Mdd\QueryBuilder;
 
-interface QueryBuilder
+class QueryBuilder
 {
+    use Traits\EscaperAware;
 
+    public function delete($table = null, $alias = null)
+    {
+        return (new Queries\Delete($table, $alias))->setEscaper($this->escaper);
+    }
+
+    public function insert($table = null)
+    {
+        return (new Queries\Insert($table))->setEscaper($this->escaper);;
+    }
+
+    public function select($columns = null)
+    {
+        return (new Queries\Select($columns))->setEscaper($this->escaper);;
+    }
+
+    public function update($table = null, $alias = null)
+    {
+        return (new Queries\Update($table, $alias))->setEscaper($this->escaper);;
+    }
 }
