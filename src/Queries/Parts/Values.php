0d90b799de7111119d2aed03de04fb4aaf3a86a3
Claude LE BRIS <claude.lebris@gmail.com>
2014-12-14 22:29:13 +0100

    Add Values part

---
+++ src/Queries/Parts/Values.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Mdd\QueryBuilder\Queries\Parts;
+
+use Mdd\QueryBuilder\PartBuilder;
+use Mdd\QueryBuilder\Type;
+use Mdd\QueryBuilder\Types;
+use Mdd\QueryBuilder\Conditions;
+use Mdd\QueryBuilder\Traits\EscaperAware;
+use Mdd\QueryBuilder\Types\String;
+
+class Values implements PartBuilder
+{
+    use EscaperAware;
+
+    private
+        $values;
+
+    public function __construct($values = null)
+    {
+        $this->values = array();
+
+        if(! empty($values))
+        {
+            $this->addValues($values);
+        }
+    }
+
+    private function addValues(array $values)
+    {
+        $this->values[] = $values;
+    }
+
+    public function toString()
+    {
+        if(empty($this->values))
+        {
+            throw new \RuntimeException('No values to insert');
+        }
+
+        $columnsNameList = array_filter(array_keys(reset($this->values)));
+
+        $values = array();
+        foreach($this->values as $valuesSet)
+        {
+            $values[] = $this->buildValuesSetString($valuesSet);
+        }
+
+        return sprintf(
+            '%s VALUES %s',
+            $this->wrapWithParentheses(implode(', ', $columnsNameList)),
+            implode(', ', $values)
+        );
+    }
+
+    private function buildValuesSetString(array $values)
+    {
+        $valuesSet = array();
+        foreach($values as $columnName => $value)
+        {
+            if(! empty($columnName))
+            {
+                $valuesSet[] = $this->formatValue($value);
+            }
+        }
+
+        return $this->wrapWithParentheses(implode(', ', $valuesSet));
+    }
+
+    private function formatValue($value)
+    {
+        if(is_null($value))
+        {
+            return 'NULL';
+        }
+
+        $type = $this->guessType($value);
+
+        return $this->escapeValue($type);
+    }
+
+    private function escapeValue(Type $type)
+    {
+        $value = $type->getValue();
+
+        if($type->isEscapeRequired())
+        {
+            $value = $this->escaper->escape($value);
+        }
+
+        return $value;
+    }
+
+    private function wrapWithParentheses($value)
+    {
+        return sprintf('(%s)', $value);
+    }
+
+    private function guessType($value)
+    {
+        if(is_int($value))
+        {
+            return new Types\Integer($value);
+        }
+
+        if(is_float($value))
+        {
+            return new Types\Float($value);
+        }
+
+        if($value instanceof \DateTime)
+        {
+            return new Types\Datetime($value);
+        }
+
+        return new Types\String($value);
+    }
+}
---
+++ tests/Queries/Parts/ValuesTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use Mdd\QueryBuilder\Queries\Parts;
+use Mdd\QueryBuilder\Tests\Escapers\SimpleEscaper;
+
+class ValuesTest extends PHPUnit_Framework_TestCase
+{
+    protected
+        $escaper;
+
+    protected function setUp()
+    {
+        $this->escaper = new SimpleEscaper();
+    }
+
+    /**
+     * @dataProvider providerTestValues
+     */
+    public function testValues($expected, array $values)
+    {
+        $part = new Parts\Values($values);
+
+        $part->setEscaper($this->escaper);
+
+        $this->assertSame($expected, $part->toString());
+    }
+
+    public function providerTestValues()
+    {
+        return array(
+            'Nominal'             => array("(id, name) VALUES (42, 'poney')", array('id' => 42, 'name' => 'poney')),
+            '1 empty column name' => array("(id, name) VALUES (42, 'poney')", array('id' => 42, 'name' => 'poney', '' => 'unicorn')),
+            'empty value'         => array("(id, name) VALUES (42, '')", array('id' => 42, 'name' => '')),
+            'null empty value'    => array("(id, name) VALUES (42, NULL)", array('id' => 42, 'name' => null)),
+            'value integer as string' => array("(id) VALUES ('42')", array('id' => '42')),
+            'value float'         => array("(id, name, score) VALUES (42, 'poney', 13.37)", array('id' => 42, 'name' => 'poney', 'score' => 13.37)),
+            'value datetime'      => array("(id, name, date) VALUES (42, 'poney', '2014-03-07 13:37:42')", array('id' => 42, 'name' => 'poney', 'date' => \Datetime::createFromFormat('Y-m-d H:i:s', '2014-03-07 13:37:42'))),
+        );
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testNoValues()
+    {
+        $part = new Parts\Values(array());
+
+        $part->setEscaper($this->escaper);
+
+        $part->toString();
+    }
+}
