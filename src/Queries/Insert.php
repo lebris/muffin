c6bceca18baef8c829d1fc0ff1f37680ced28a26
Claude LE BRIS <claude.lebris@gmail.com>
2014-12-14 23:02:14 +0100

    Add Insert query

---
+++ src/Queries/Insert.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Mdd\QueryBuilder\Queries;
+
+use Mdd\QueryBuilder\Query;
+use Mdd\QueryBuilder\Condition;
+use Mdd\QueryBuilder\Traits\EscaperAware;
+use Mdd\QueryBuilder\PartBuilder;
+use Mdd\QueryBuilder\Queries\Parts\TableName;
+
+class Insert implements Query
+{
+    use EscaperAware;
+
+    private
+        $insertPart,
+        $valuesPart;
+
+    public function __construct($table)
+    {
+        $this->valuesPart = new Parts\Values();
+
+        if(! empty($table))
+        {
+            $this->insertPart = new Parts\Insert($table);
+        }
+    }
+
+    public function toString()
+    {
+        $queryParts = array(
+            $this->insertPart->toString(),
+            $this->buildValuesString(),
+        );
+
+        return implode(' ', $queryParts);
+    }
+
+    public function insert($table)
+    {
+        $this->insertPart = new Parts\Insert($table);
+
+        return $this;
+    }
+
+    public function values(array $values)
+    {
+        $this->valuesPart->values($values);
+
+        return $this;
+    }
+
+    private function buildValuesString()
+    {
+        $this->valuesPart->setEscaper($this->escaper);
+
+        return $this->valuesPart->toString();
+    }
+}
---
+++ tests/Queries/InsertTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use Mdd\QueryBuilder\Queries;
+use Mdd\QueryBuilder\Conditions;
+use Mdd\QueryBuilder\Types;
+use Mdd\QueryBuilder\Tests\Escapers\SimpleEscaper;
+
+class InsertTest extends PHPUnit_Framework_TestCase
+{
+    protected
+        $escaper;
+
+    protected function setUp()
+    {
+        $this->escaper = new SimpleEscaper();
+    }
+
+    public function testInsertUsingConstructor()
+    {
+        $query = (new Queries\Insert('burger'))->setEscaper($this->escaper);
+
+        $query->values(array(
+            'id' => 666,
+            'name' => 'poney',
+            'date' => \Datetime::createFromFormat('Y-m-d H:i:s', '2017-03-07 13:37:42'),
+            'score' => 13.37,
+        ));
+
+        $this->assertSame("INSERT INTO burger (id, name, date, score) VALUES (666, 'poney', '2017-03-07 13:37:42', 13.37)", $query->toString($this->escaper));
+
+        $query->values(array(
+            'id' => 667,
+            'name' => 'unicorn',
+            'date' => \Datetime::createFromFormat('Y-m-d H:i:s', '2017-03-07 13:42:59'),
+            'score' => 14.18,
+        ));
+
+        $this->assertSame("INSERT INTO burger (id, name, date, score) VALUES (666, 'poney', '2017-03-07 13:37:42', 13.37), (667, 'unicorn', '2017-03-07 13:42:59', 14.18)", $query->toString($this->escaper));
+    }
+}
