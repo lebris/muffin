d6e47c855f08191e7811b454f5b39788ee533afa
Claude LE BRIS <claude.lebris@gmail.com>
2014-12-14 20:03:12 +0100

    Add Delete query

---
+++ src/Queries/Delete.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Mdd\QueryBuilder\Queries;
+
+use Mdd\QueryBuilder\Query;
+use Mdd\QueryBuilder\PartBuilders;
+use Mdd\QueryBuilder\Condition;
+use Mdd\QueryBuilder\Escaper;
+use Mdd\QueryBuilder\Traits\EscaperAware;
+use Mdd\QueryBuilder\PartBuilder;
+
+class Delete implements Query
+{
+    use EscaperAware;
+
+    private
+        $from,
+        $where;
+
+    public function __construct()
+    {
+        $this->where = new Parts\Where();
+    }
+
+    public function toString()
+    {
+        $queryParts = array(
+            'DELETE',
+            $this->buildFrom(),
+            $this->buildWhere(),
+        );
+
+        return implode(' ', array_filter($queryParts));
+    }
+
+    public function from($table, $alias = null)
+    {
+        $this->from = new Parts\From($table, $alias);
+
+        return $this;
+    }
+
+    public function where(Condition $condition)
+    {
+        $this->where->where($condition);
+
+        return $this;
+    }
+
+    private function buildFrom()
+    {
+        if(!$this->from instanceof PartBuilder)
+        {
+            throw new \LogicException('No column for FROM clause');
+        }
+
+        return $this->from->toString();
+    }
+
+    private function buildWhere()
+    {
+        $this->where->setEscaper($this->escaper);
+
+        return $this->where->toString();
+    }
+}
---
+++ tests/Queries/DeleteTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Mdd\QueryBuilder\Queries;
+use Mdd\QueryBuilder\Conditions;
+use Mdd\QueryBuilder\Types;
+use Mdd\QueryBuilder\Tests\Escapers\SimpleEscaper;
+
+class DeleteTest extends PHPUnit_Framework_TestCase
+{
+    protected
+        $escaper;
+
+    protected function setUp()
+    {
+        $this->escaper = new SimpleEscaper();
+    }
+
+    public function testDeleteSingleTable()
+    {
+        $query = (new Queries\Delete())->setEscaper($this->escaper);
+
+        $query
+            ->from('burger')
+            ->where(new Conditions\Equal('type', new Types\String('healthy')))
+        ;
+
+        $this->assertSame("DELETE FROM burger WHERE type = 'healthy'", $query->toString($this->escaper));
+    }
+}
